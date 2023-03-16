372985bbd462205fc803fc8b3a603b8d227db91b
Erick Awino <erick.otieno89@gmail.com>
2023-03-16 21:56:35 +0300

    Size is not granduer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - the main function
+* Return: zero on success
+*/
+int main(void)
+{
+	printf("Size of a char: %lu  byte(s)\n", sizeof(char));
+	printf("Size of a int: %lu  byte(s)\n", sizeof(char));
+	printf("Size of a long int: %lu  byte(s)\n", sizeof(long int));
+	printf("Size of a long long int:%lu  byte(s)\n", sizeof(long long int));
+	printf("Size of a float:%lu  byte(s)\n", sizeof(float));
+	return (0);
+
+}
+
