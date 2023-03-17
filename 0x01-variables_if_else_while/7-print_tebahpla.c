868ef38a2054877b0f858ebd98322988eba011dc
Erick Awino <erick.otieno89@gmail.com>
2023-03-18 00:17:36 +0300

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - main function (entry point).
+*
+* Return: always 0 (success)
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
