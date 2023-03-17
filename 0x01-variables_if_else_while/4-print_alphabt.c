8d10cd6f66742f26981e96f97e41779df8f0751b
Erick Awino <erick.otieno89@gmail.com>
2023-03-17 23:48:45 +0300

    Alphabet in lower case, task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - main function (Entry Point).
+*
+* Return: always 0 (success).
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
