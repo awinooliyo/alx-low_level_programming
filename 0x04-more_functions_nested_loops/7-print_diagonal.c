f75a760a1b5f9f14ca72b19e9d2f0271bdd70236
Erick Awino <erick.otieno89@gmail.com>
2023-03-23 23:02:12 +0300

    Prints diagonal line on a terminal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,20 +11,20 @@ void print_diagonal(int n)
 	{
 		_putchar('\n');
 	}
-	else 
+	else
 	{
 		int i, j;
 
 		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < n; j++)
-				{
-					if (j == i)
-						_putchar('\\');
-					else if (j < i)
-						_putchar(' ');
-				}
-				_putchar('\n');
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
 		}
 	}
 }
