437dbfbaec491a4f9f3b1e54c2ccdb67a0f56d2b
Erick Awino <erick.otieno89@gmail.com>
2023-03-28 01:34:19 +0300

    changes

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -16,6 +17,7 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+void print_array(int *a, int n);
 
 
 #endif
