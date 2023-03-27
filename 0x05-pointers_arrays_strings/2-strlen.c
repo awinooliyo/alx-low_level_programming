10772bed1538de5c2494cec2c722cd15cee9a39e
Erick Awino <erick.otieno89@gmail.com>
2023-03-27 21:22:40 +0300

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strlen - returns the length of a string.
+*
+* @s: string.
+* Return: length of string.
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
