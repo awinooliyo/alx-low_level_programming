242fda22d6a8b340fa4415648f51fd56ab7dddb1
Erick Awino <erick.otieno89@gmail.com>
2023-03-31 05:15:28 +0300

    Solution 9: Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,17 +11,16 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-	n1 = -n;
-	_putchar('-');
-	}
-	else
+		n1 = -n;
+		_putchar('-');
+	} else
 	{
-	n1 = n;
+		n1 = n;
 	}
-	while (n1 / 10)
-	{
-	print_number(n1 / 10);
-	}
+
+	if (n1 / 10)
+		print_number(n1 / 10);
 
 	_putchar((n1 % 10) + '0');
 }
+
