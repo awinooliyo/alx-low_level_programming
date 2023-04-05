64d677bd45450da279ae80a0f9398fe4143ef5fc
Erick Awino <erick.otieno89@gmail.com>
2023-04-05 23:48:35 +0300

    Task 7 (Q6) Recursion tasks

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,22 +8,50 @@
 
 int is_prime_number(int n)
 {
-	int j;
+	int prime_number(int divider, int n)
 
-	if (n <= 1)
+	int divider = 2;
+
+	if (n < 2)
 	{
 		return (0);
 	}
-	else if (j > n / 2)
+	if (n % n == 0 || n % 1 == 0)
 	{
-		return (1);
+		if (prime_number(divider, n) != 0)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
 	}
-	else if (n % j == 0)
+}
+
+/**
+* prime_number - helper function for the recursive steps.
+* @n: number given to the original function.
+* @divider: incrementer divisor.
+* Return: 0 if not a prime number, 1 if a prime number.
+*/
+
+int prime_number(int divider, int n)
+{
+	if (divider < n)
 	{
-		return (0);
+		if (n % divider == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			++divider;
+			return (prime_number(divider, n));
+		}
 	}
 	else
 	{
-		return (is_prime_number(n, j + 1));
+		return (1);
 	}
 }
