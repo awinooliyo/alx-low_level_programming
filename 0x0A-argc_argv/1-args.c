6b6c4ccee02266cbecc73dbfe64eef098fb340d8
Erick Awino <erick.otieno89@gmail.com>
2023-04-10 14:47:36 +0300

    Task 1 solution argc & argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+* main - prints the number of arguments passed to it.
+* @argc: the number of arguments passed to the program.
+* @argv: the array of pointers to the arguments passed.
+*
+* Return: always 0 on success.
+*/
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
