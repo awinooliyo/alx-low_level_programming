5e102f25dcb8ec7fadc5d8753f14e7f47ef7333e
Erick Awino <erick.otieno89@gmail.com>
2023-04-11 17:33:39 +0300

    Task 1 (no.0) solution for malloc, free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* create_array - create an array of chars,
+*           and initializes it with specific char.
+* @size: the size of the array being created.
+* @c: the char to initialize the array c.
+* Return: pointer to the array if successful, otherwise NULL.
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i = 0;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = (char *) malloc(sizeof(char) * size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+The repository for tasks relating to 0x0B. C - malloc, free.
