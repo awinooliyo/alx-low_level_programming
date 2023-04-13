0317e9d3c3e134a4a8d8bca9d7fad3ebb50cfedb
Erick Awino <erick.otieno89@gmail.com>
2023-04-14 01:06:43 +0300

    Task number 2 solution

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,8 +5,9 @@
 * _calloc - allocates memory of an array using malloc.
 * @nmemb: number of elements in the array.
 * @size: size of elememts in the array.
-* Return: Null when size of nmemb is 0 or malloc fails and
-*	pointer to memory allocated when successful.
+* Return: Null when size of nmemb is 0
+* Null when  malloc fails, and
+* Pointer to memory allocated if successful.
 */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
