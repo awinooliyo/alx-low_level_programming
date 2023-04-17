76ca8fcc3b503e1cdef310989ef012bd456b1638
Erick Awino <erick.otieno89@gmail.com>
2023-04-17 20:43:25 +0300

    Task 0 Solution

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - basic info of a dog.
+* @name: the dog's name of char data type
+* @age: The dog's age of float data type.
+* @owner: the name of the dog's owner.
+*
+* Description: No description.
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
