9a90e4c6e7b5ca7de9bfb0ca47b92c238eec2949
Erick Awino <erick.otieno89@gmail.com>
2023-05-08 18:02:01 +0300

    Solution for question number 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,22 +10,27 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t  bytes_written = 0;
+	int number_of_letters = 0;
+	int rwr;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
-	{
-		bytes_written = write(fd, text_content, strlen(text_content));
-		if (bytes_written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	while (text_content[number_of_letters])
+		number_of_letters++;
+
+	rwr = write(fd, text_content, number_of_letters);
+
+	if (rwr == -1)
+		return (-1);
 
 	close(fd);
 	return (1);
