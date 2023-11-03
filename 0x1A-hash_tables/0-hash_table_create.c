f0604809d49b7a4d23aab4350c4529777224d0ae
Erick Awino <erick.otieno89@gmail.com>
2023-11-03 20:47:25 +0300

    a function that creates a hash table.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -27,7 +27,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_new->array = calloc((size_t)size, sizeof(hash_node_t *));
 
 	if (hash_table_new->array == NULL)
+	{
+		free(hash_table_new);
 		return NULL;
-
+	}
 	return (hash_table_new);
 }
