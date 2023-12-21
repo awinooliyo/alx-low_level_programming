148bd3e9a54fac8bf731fd31f8e8213f737c538c
Erick Awino <erick.otieno89@gmail.com>
2023-12-21 01:22:11 -0800

    island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module that calculates the perimeter of an island in a grid"""
 
+
 def island_perimeter(grid):
     """
     Returns the perimeter of the island described in the groid
@@ -12,10 +13,10 @@ def island_perimeter(grid):
     size = 0
 
     for x in range(height):
-        for y in range (width):
+        for y in range(width):
             if grid[x][y] == 1:
                 size += 1
-                if (y > 0 and grid [x][y - 1] == 1):
+                if (y > 0 and grid[x][y - 1] == 1):
                     edges += 1
                 if (x > 0 and grid[x - 1][y] == 1):
                     edges += 1
