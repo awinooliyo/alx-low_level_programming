45b96928d954a4604a07907b34b91d01202ec949
Erick Awino <erick.otieno89@gmail.com>
2024-02-06 19:01:32 +0300

    Task 1 Advanced

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,11 +3,11 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 
 /* Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
+int jump_search(int *array, size_t size, int value);
 
 #endif
